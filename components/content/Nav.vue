209ea32e17baf7aa7536d5f0653025d8231e4f55
Berry Roelfsema <broelfsema@offertvergelijker.nl>
2024-12-13 09:04:27 +0100

    update

--- components/content/Nav.vue
+++ components/content/Nav.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="bg-green-500 py-4">
     <div class="container bg-green-300 flex flex-wrap justify-between gap-y-4">
-      <ContentSlot name="logo-col"/>
-      <ContentSlot name="navigation-col"/>
-      <ContentSlot name="action-col"/>
+      <ContentSlot />
     </div>
   </div>
 </template>
