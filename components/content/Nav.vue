cebc20bd3556817f4f78a6c39d38898328ab7ba0
Berry Roelfsema <broelfsema@offertvergelijker.nl>
2024-12-13 09:46:23 +0100

    update

--- components/content/Nav.vue
+++ components/content/Nav.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="bg-green-500 py-4">
     <div class="container bg-green-300 flex flex-wrap justify-between gap-y-4">
-      <ContentSlot name="logo-col"/>
-      <ContentSlot name="navigation-col"/>
-      <ContentSlot name="action-col"/>
+      <ContentSlot :default="logoDefault" name="logo-col"/>
+      <ContentSlot :default="navigationDefault" name="navigation-col"/>
+      <ContentSlot :default="actionDefault" name="action-col"/>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+const logoDefault = {
+  logo: '/path-to-logo.png',
+  link: '/'
+}
 
+const navigationDefault = {
+  links: [
+    { text: 'Home', url: '/' },
+    { text: 'About', url: '/about' }
+  ]
+}
+
+const actionDefault = {
+  buttons: [
+    { text: 'Contact', url: '/contact', style: 'primary' }
+  ]
+}
 </script>
