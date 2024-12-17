ba29c22b4400591741f79a860b1ab21eb49acc18
Berry Roelfsema <broelfsema@offertvergelijker.nl>
2024-12-17 13:30:52 +0100

    update

--- components/content/column/Column.vue
+++ components/content/column/Column.vue
@@ -1,7 +1,9 @@
 <template>
   <div :class="`bg-red-500 ${getWidth(width)} ${padding}`">
     <ColumnContainer>
-      <slot />
+      <div v-if="$slots.icon"><ContentSlot name="icon" unwrap="p"/></div>
+      <p v-if="$slots.content"><ContentSlot name="content" unwrap="p"/></p>
+      <button v-if="$slots.button"><ContentSlot name="button" unwrap="p"/></button>
     </ColumnContainer>
   </div>
 </template>
