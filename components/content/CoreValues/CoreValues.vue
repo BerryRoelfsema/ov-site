2877b68c44fe47dbcbeeb741aaa676f707cc13bc
Berry Roelfsema <broelfsema@offertvergelijker.nl>
2024-12-20 10:12:45 +0100

    update

--- components/content/CoreValues/CoreValues.vue
+++ components/content/CoreValues/CoreValues.vue
@@ -3,16 +3,16 @@
     <SectionContainer :width="containerWidth" class="bg-primary-900">
       <div class="w-full">
         <div class="container flex flex-row justify-between text-center text-white">
-          <p class="flex flex-row gap-x-1 items-center before:bg-gray-50 before:absolute before:aspect-square before:w-4 before:ml-0.5 before:rounded-full">
+          <p v-if="$slots.valueOne" class="flex flex-row gap-x-1 items-center before:bg-gray-50 before:absolute before:aspect-square before:w-4 before:ml-0.5 before:rounded-full">
             <Icon name="mdi:checkbox-marked-circle" size="20" class="bg-green-500"/><ContentSlot name="valueOne" unwrap="p"/>
           </p>
-          <p class="flex flex-row gap-x-1 items-center before:bg-gray-50 before:absolute before:aspect-square before:w-4 before:ml-0.5 before:rounded-full">
+          <p v-if="$slots.valueTwo" class="flex flex-row gap-x-1 items-center before:bg-gray-50 before:absolute before:aspect-square before:w-4 before:ml-0.5 before:rounded-full">
             <Icon name="mdi:checkbox-marked-circle" size="20" class="bg-green-500"/><ContentSlot name="valueTwo" unwrap="p"/>
           </p>
-          <p class="flex flex-row gap-x-1 items-center before:bg-gray-50 before:absolute before:aspect-square before:w-4 before:ml-0.5 before:rounded-full">
+          <p v-if="$slots.valueThree" class="flex flex-row gap-x-1 items-center before:bg-gray-50 before:absolute before:aspect-square before:w-4 before:ml-0.5 before:rounded-full">
             <Icon name="mdi:checkbox-marked-circle" size="20" class="bg-green-500"/><ContentSlot name="valueThree" unwrap="p"
           /></p>
-          <p class="flex flex-row gap-x-1 items-center before:bg-gray-50 before:absolute before:aspect-square before:w-4 before:ml-0.5 before:rounded-full">
+          <p v-if="$slots.valueFour" class="flex flex-row gap-x-1 items-center before:bg-gray-50 before:absolute before:aspect-square before:w-4 before:ml-0.5 before:rounded-full">
             <Icon name="mdi:checkbox-marked-circle" size="20" class="bg-green-500"/><ContentSlot name="valueFour" unwrap="p"/>
           </p>
         </div>
